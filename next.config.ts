df4ecc184efd743a369c5bc2a2f173eb28b78ede
Krishna <krishna@Krishnas-MacBook-Air.local>
2026-03-09 17:00:50 +0530

    solved

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
   images: {
-        remotePatterns: [
-          {
-            protocol: 'https', // or 'http' if applicable
-            hostname: 'qcbucket-971565583415-ap-south-1.s3.ap-south-1.amazonaws.com', // Replace with the actual domain of your images
-           
-    
-          },
-      
-        ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'qcbucket-971565583415-ap-south-1.s3.ap-south-1.amazonaws.com',
       },
+    ],
+  },
 };
 
 export default nextConfig;
